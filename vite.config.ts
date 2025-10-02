4a380abf782f479142f032ec9b3b4ad9528dfab5
nico <ti57358@gmail.com>
2025-10-02 15:41:01 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './',   // ← これを追加
       server: {
         port: 3000,
         host: '0.0.0.0',
